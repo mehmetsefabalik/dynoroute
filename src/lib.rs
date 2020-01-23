4ad64b4c1183890bd374840a3f13a7e073bd1851
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-01-23 23:46:39 +0300

    library description comment is added

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+/*
+ dynoroute will provide a solution for creating dynamically routed applications.
+ To specify, web servers will be able to change its routing on runtime.
+*/
+
 #[cfg(test)]
 mod tests {
     #[test]
